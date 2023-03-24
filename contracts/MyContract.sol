43753004888ac7c9204c1e4fe75e7f2d591475bc
Jonathan Rodriguez <jonathanrodriguez@192.168.1.4>
2023-03-23 19:48:29 -0600

    create logical smart contract

---
+++ contracts/MyContract.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+contract MyContract {
+    address[16] public perrosAdoptados;
+
+    function adoptar(uint perroIndex) public returns (bool){
+        require(perroIndex >= 0 && perroIndex <= 15 , "Fuera de rango" );
+
+        bool adoptarConExito = true;
+
+        if (perrosAdoptados[perroIndex] == address(0)) {
+            perrosAdoptados[perroIndex] = msg.sender;
+        } else {
+            adoptarConExito = false;
+        }
+
+        return adoptarConExito;
+    }
+
+    function getPerrosAdoptados() public view returns (address[16] memory){
+        return perrosAdoptados;
+    }
+}
